0a2b065c591182c216e18df2ad7c8de74a506081
Matt Campbell <cmampbell@gmail.com>
2022-12-20 17:39:47 -0500

    refactored and did some styling. everything is still working. need to add tests

--- app.py
+++ app.py
@@ -13,24 +13,40 @@ boggle_game = Boggle()
     #contains button with get action to the game page
 @app.route('/')
 def show_game_page():
-    '''View function that will render the template for the Boggle game,
-    and set the game board in the session'''
+    '''Will generate a game board, save it to the session, check session cookies for
+    users high score, number of games played, and set values in the session if they
+    do not exist.'''
     session['game_board'] = boggle_game.make_board()
+    session['found_words'] = []
 
+    check_session_on_start()
+
+    return render_template('game_page.html', game_board = session['game_board'], games_played=session['games_played'], high_score=session['high_score'], found_words=session['found_words'])
+
+def check_session_on_start():
+    '''Check session to see if high score and games played values exist. If not, make them'''
     if session.get('high_score') is None:
         session['high_score'] = 0
 
     if session.get('games_played') is None:
         session['games_played'] = 0
 
-    return render_template('game_page.html', game_board = session['game_board'], games_played=session['games_played'], high_score=session['high_score'])
-
 @app.route('/check')
 def check_guess():
     '''Route that will take the user input from site and check against database of words'''
     # get the users guess
     user_guess = request.args.get('guess')
-    return jsonify({'result': boggle_game.check_valid_word(session['game_board'], user_guess)})
+
+    if user_guess in session['found_words']:
+        return jsonify({'result': 'already found'})
+    else:
+        add_guess_to_session(user_guess)
+        return jsonify({'result': boggle_game.check_valid_word(session['game_board'], user_guess), 'found_words': session['found_words']})
+
+def add_guess_to_session(user_guess):
+        found_words = session['found_words']
+        found_words.append(user_guess)
+        session['found_words'] = found_words
 
 @app.route('/end-game')
 def end_game():
@@ -51,3 +67,4 @@ def end_game():
     
     
     return jsonify({'games_played': session['games_played'], 'high_score': user_score})
+
--- static/boggle.js
+++ static/boggle.js
@@ -1,59 +1,56 @@
-$startButton = $('#start-button')
-$rules = $('#rules')
 $game = $('#game')
 $userGuess = $('#guess');
-$guessForm = $('#guess-form')
-$submitGuess = $('.submit-guess');
-$currentScore = $('#current-score');
-$guessMessage = $('.guess-message');
 $endGame = $('#end-game');
-$playAgain = $('#play-again');
 
 $game.hide()
 $endGame.hide()
 score = 0;
 
-function startGame(){
-    //show game board
-    $game.show()
-    $rules.hide()
-    //start timer
-    setTimeout(async function(){
-        //hide the submit guess button
-        $guessForm.hide()
+class BoggleGame{
+    static startGame(){
+        //show game board
+        $game.show()
+        $('#rules').hide()
+
+        //start game timer
+        setTimeout(BoggleGame.gameTimerEnd, 60000)
+    }
+
+    static async handleGuess(evt){
+        //prevent page from refreshing
+        evt.preventDefault();
+    
+        // store users guess in a variable
+        const guess = $userGuess.val();
+    
+        //clear the value for guess input
+        $userGuess.val('');
+    
+        //make axios request to the server
+        const resp = await axios.get('/check', {
+            params: { 'guess': guess }
+        })
+    
+        //use server response to flash message to user
+        $('.guess-message').text(`${guess} is ${resp.data.result}`)
+    
+        if(resp.data.result == 'ok'){
+            score += guess.length
+            $('#current-score').text(score)
+            $('#found-words-list').append(`<li>${guess}</li>`)
+        }
+    }
+
+    static async gameTimerEnd(){
+        //hide the submit guess form so user can't submit guesses
+        $('#guess-form').hide()
         $endGame.show()
 
         //send request to server with high score
         axios.get('/end-game', {params: {'score': score }})
-    }, 30000)
-}
-
-$startButton.click(startGame);
-
-async function handleGuess(evt){
-    //prevent page from refreshing
-    evt.preventDefault();
-
-    // store users guess in a variable
-    guess = $userGuess.val();
-
-    //clear the value for guess input
-    $userGuess.val('');
-
-    //make axios request to the server
-    resp = await axios.get('/check', {
-        params: { 'guess': guess }
-    })
-
-    console.log(resp)
-
-    //use server response to flash message to user
-    $guessMessage.text(`${guess} is ${resp.data.result}`)
-
-    if(resp.data.result == 'ok'){
-        score += guess.length
-        $currentScore.text(score)
     }
 }
 
-$submitGuess.click(handleGuess);
+$('#start-button').click(BoggleGame.startGame);
+
+$('#submit-guess').click(BoggleGame.handleGuess);
